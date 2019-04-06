6d180002277105b7be2ae1a301705cdf67de1d7b
Shubhra1028 <shubhra1028@gmail.com>
2019-04-06 15:08:32 +0530

    structure setup

--- test.c
+++ test.c
@@ -1,34 +1,39 @@
 #include<stdio.h>
 
+struct process
+{
+    char name[2];
+    int arrivalTime, burstTime, completedAt, turnAroundTime, avgWaitingTime;
+};
+
+
 int main(){
-    int Q1[100], Q2[100];
+    struct process Q1[100], Q2[100];
     int f, at, bt, n, q1=0, q2=0;
     printf("Enter number of process : ");
     scanf("%d", &n);
     for(int i = 0; i < n; i++)
     {
-        printf("Process 1 is in Queue 1 ? (1 for yes 0 for no) ");
+        printf("Process %d is in Queue 1 ? (1 for yes 0 for no) ", i);
         scanf("%d", &f);
-        printf("Process 1 burst Time");
+        printf("Process %d burst Time : ", i);
         if(f){
-            scanf("%d", &Q1[q1]);
-            q1++;
+            scanf("%d", &Q1[q1++].burstTime);
         }else
         {
-            scanf("%d", &Q2[q2]);
-            q2++;
+            scanf("%d", &Q2[q2++].burstTime);
         }
         
     }
     printf("\nQueue 1\n");
     for(int i = 0; i < q1; i++)
     {
-        printf("%d ", Q1[i]);
+        printf("%d ", Q1[i].burstTime);
     }
     printf("\nQueue 2\n");
     for(int i = 0; i < q2; i++)
     {
-        printf("%d ", Q2[i]);
+        printf("%d ", Q2[i].burstTime);
     }
     
     
